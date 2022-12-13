18155b0aa995b47f3d4ab62e2e67c33e215808e2
Changer_NoteBook <changerqi@163.com>
2022-12-13 17:10:29 +0800

    2022/12/13 pm,用Chatgpt修改了乘法口诀

--- test_chengfa.go
+++ test_chengfa.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	for i := 1; i < 10; i++ {
-		var s string
-		for j := 1; j < 10; j++ {
-			if j > i {
-				break
-			}
-			s += strconv.Itoa(j) + "*" + strconv.Itoa(i) + "=" + strconv.Itoa(i*j) + " "
+	for i := 1; i <= 9; i++ {
+		// var s string
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %d \t", j, i, i*j)
+			// if j > i {
+			// 	break
+			// }
+			// s += strconv.Itoa(j) + "*" + strconv.Itoa(i) + "=" + strconv.Itoa(i*j) + " "
 		}
-		s = s + "\n"
-		fmt.Println(s)
+		// s = s + "\n"
+		fmt.Println()
 	}
 }
